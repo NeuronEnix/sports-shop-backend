6e80ecdd27a837c7bb81816d7cd5b4df87ddeab5
NEURON <50287102+neuron909@users.noreply.github.com>
2020-12-27 22:04:19 +0530

    Removed: validation -> user route

--- app/user/user.router.js
+++ app/user/user.router.js
@@ -3,8 +3,8 @@ const router = require( 'express' ).Router() ;
 const user = require( './user.controller' ) ;
 const { validator } = require( "../../middleware/validator" ) ;
 
-router.post( '/sign-up' , validator.user.signUp  , user.signUp  ) ;
-router.post( '/sign-in' , validator.user.signIn  , user.signIn  ) ;
-router.get ( '/sign-out', validator.user.signOut , user.signOut ) ;
+router.post( '/sign-up'   , user.signUp  ) ;
+router.post( '/sign-in'   , user.signIn  ) ;
+router.get ( '/sign-out' , user.signOut ) ;
 
 module.exports.router = router
