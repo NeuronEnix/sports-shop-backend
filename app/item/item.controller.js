a74deb9c2bb731689769c5deadf7fda8a0b07d4d
Sasuke-003 <56964273+Sasuke-003@users.noreply.github.com>
2020-12-31 14:54:56 +0530

    changed sorting order in home

--- app/item/item.controller.js
+++ app/item/item.controller.js
@@ -1,46 +1,39 @@
-const Item    = require( './item.model' ) ;
-const respond = require( '../../response' ) ;
-const fs = require( "fs" );
+const Item = require("./item.model");
+const respond = require("../../response");
+const fs = require("fs");
 
-module.exports.addItem = async ( req, res ) => {
-    const itemData  = { ...req.body, ...{ user_id:req.UserID } } ;
+module.exports.addItem = async (req, res) => {
+    const itemData = { ...req.body, ...{ user_id: req.UserID } };
     // console.log( { itemData })
     // if( !req.files )
     //     return respond.err( res, { err: respond.errData.dbCommitErr, info: "Supports only .png images"} );
     const newItem = new Item();
-    Object.assign( newItem, itemData );
+    Object.assign(newItem, itemData);
     try {
-
         await newItem.save();
-        return respond.ok( res );
-
-    } catch ( err ) {
-        return respond.err( res, {err:respond.errData.duplicateErr, info: "Already Exist: " + itemData.name } );
+        return respond.ok(res);
+    } catch (err) {
+        return respond.err(res, { err: respond.errData.duplicateErr, info: "Already Exist: " + itemData.name });
     }
-}
+};
 
+module.exports.listItem = async (req, res) => {
+    const itemDocs = await Item.find({ status: "a" }, { _id: 0, __v: 0, status: 0, user_id: 0 }).sort({ _id: 1 });
+    return respond.ok(res, itemDocs);
+};
 
-module.exports.listItem = async( req, res) => {
-    const itemDocs = await Item.find( {status:"a"}, { _id:0, __v:0, status:0, user_id:0 } ).sort({name:1})
-    return respond.ok( res, itemDocs );
-}
-
-module.exports.getImgLink = async ( req, res ) => {
+module.exports.getImgLink = async (req, res) => {
     const itemName = req.query.name;
     const imgPath = __dirname + "\\img\\" + itemName + ".png";
-    
-    if( fs.existsSync( imgPath ))
-        return respond.ok( res, `${req.connection.localAddress}:${req.connection.localPort}/item/img/${itemName}.png` )   ;
-    return respond.err( res, { err: respond.errData.resNotFound } );
-}
 
-module.exports.detail = async ( req, res ) => {
+    if (fs.existsSync(imgPath)) return respond.ok(res, `${req.connection.localAddress}:${req.connection.localPort}/item/img/${itemName}.png`);
+    return respond.err(res, { err: respond.errData.resNotFound });
+};
+
+module.exports.detail = async (req, res) => {
     const itemName = req.query.name;
-    
-    const itemDetail = await Item.findOne( 
-        { name:itemName, status:'a' },
-        { _id:0, size_identifier_qty:1 }
-    );
 
-    return respond.ok( res, itemDetail );
-}
+    const itemDetail = await Item.findOne({ name: itemName, status: "a" }, { _id: 0, size_identifier_qty: 1 });
+
+    return respond.ok(res, itemDetail);
+};
--- app/order/order.model.js
+++ app/order/order.model.js
@@ -1,43 +1,44 @@
-const mongoose = require( 'mongoose' ) ;
-const moment = require( "moment" ) ;
+const mongoose = require("mongoose");
+const moment = require("moment");
 
-const Item = require( "mongoose").model( "items" );
-const errData  = require( '../../response.js' ).errData ;
+const Item = require("mongoose").model("items");
+const errData = require("../../response.js").errData;
 
-var orderSchema = new mongoose.Schema ({
-    items : {},
-    pin : String,
-    address: String,
-    city: String,
-    statusUpdateDates:[Date],
-    user_id : { type : mongoose.Schema.Types.ObjectId, required : true },
-    status : { type : Number, default :0 },     // 'a' -> active ; 'd' -> disabled
-},
-{ timestamps: { createdAt:'created_at', updatedAt: 'updated_at'} }
+var orderSchema = new mongoose.Schema(
+    {
+        items: {},
+        pin: String,
+        address: String,
+        city: String,
+        statusUpdateDates: [Date],
+        user_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+        status: { type: Number, default: 0 }, // 'a' -> active ; 'd' -> disabled
+    },
+    { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
-orderSchema.statics.CreateOrder = async ( orderDetails = {}, user_id ) => {
+orderSchema.statics.CreateOrder = async (orderDetails = {}, user_id) => {
     const orderDoc = new Order();
-    const dates = getDates();;
-    console.log( dates )
+    const dates = getDates();
+    console.log(dates);
     orderDoc.statusUpdateDates = dates;
     orderDoc.user_id = user_id;
-    Object.assign( orderDoc, orderDetails)
+    Object.assign(orderDoc, orderDetails);
     orderDoc.save();
-}
+};
 
-
-const Order = mongoose.model( 'orders', orderSchema ) ;
+const Order = mongoose.model("orders", orderSchema);
 module.exports = Order;
 
 function getDates() {
     let maxDays = 10;
     const dates = [];
     const curDate = moment();
-    for( let i=0; i<3; ++i ) {
-        maxDays = Math.floor( Math.random() * maxDays )
-        console.log( maxDays );
-        dates.push( curDate.add( maxDays, 'd' ).clone() );
+    curDate.add(1, "d");
+    for (let i = 0; i < 3; ++i) {
+        maxDays = Math.floor(Math.random() * maxDays);
+        console.log(maxDays);
+        dates.push(curDate.add(maxDays, "d").clone());
     }
-    return dates;    
+    return dates;
 }
